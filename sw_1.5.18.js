ebc49f88ecfcba7e12147717be46ab9419ef26b7
ubg42 <redtomatotiger@gmail.com>
2026-07-06 07:57:07 +1000

    Implement fetch event listener for caching
    Add fetch event listener to handle caching logic.

--- sw_1.5.18.js
+++ sw_1.5.18.js
@@ -15,3 +15,28 @@ self.addEventListener('activate',function(event){
 
 });
 
+self.addEventListener('fetch', function(event) {
+    
+   const url = event.request.url;
+
+   const skipList = ['imasdk.googleapis.com','pagead2.googlesyndication.com',
+   'hb.improvedigital.com','pub.headerlift.com','s0.2mdn.net','g.doubleclick.net','tpc.googlesyndication.com','prebid','ga-audiences',
+   'tag.atom.gamedistribution.com','msgrt.gamedistribution.com','game.api.gamedistribution.com','html5.api.gamedistribution.com'];
+
+   const doNotCache = skipList.some(d=>url.indexOf(d)>=0)
+
+   if(doNotCache){
+    // console.log('GD-SW not caching url',url);
+    return;
+   }
+   
+    event.respondWith(async function() {
+       try{
+         var res = await fetch(event.request);
+         return res;
+       }
+       catch(error){
+         return null;
+        }
+      }());
+  });
